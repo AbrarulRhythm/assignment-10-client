b9b1c3a11b89d83de5de5d4ffaa5a3b211d55ac8
Abrarul Rhythm <[....email>
2025-11-10 00:24:19 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
+
+// Image
+import logoDark from '../../assets/logo-dark.png';
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
     return (
-        <div>
-            This is Header
+        <div className='container'>
+            <nav className='priamry-menu'>
+                <div className='flex flex-wrap -mx-3 items-center'>
+                    <div className='w-6/12 lg:w-3/12 px-3'>
+                        {/* Header Logo */}
+                        <Link to='/' title='PlateShare'>
+                            <img src={logoDark} alt='Header Logo' />
+                        </Link>
+                        {/* Header Logo End */}
+                    </div>
+
+                    <div className='w-auto lg:w-6/12 px-3'>
+                        {/* <button>
+                            <FaBars />
+                        </button> */}
+
+                        <div className='header-nav'>
+                            <ul>
+                                <li>
+                                    <NavLink to='/'>Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/z'>Available Foods</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/b'>About Us</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/c'>News</NavLink>
+                                </li>
+                            </ul>
+                            {/* <Link to='/' className='button button-sm'>Sign In</Link> */}
+                        </div>
+                    </div>
+
+                    <div className='w-auto lg:w-3/12 px-3'>
+                        <div className='flex items-center justify-end'>
+                            <Link to='/' className='button button-sm'>Sign In</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
