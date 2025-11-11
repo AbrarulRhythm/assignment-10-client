ce9cee918e46c89a6fe1a757da38a3933bca3bef
Abrarul Rhythm <[....email>
2025-11-12 04:16:30 +0600

    added food request modal and functionality

--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link, useLoaderData } from 'react-router';
 import { FaArrowLeftLong } from 'react-icons/fa6';
 
@@ -8,9 +8,47 @@ import food_id from '../../assets/food_id.png';
 import food_quantity from '../../assets/food_quantity_lg.png';
 import location from '../../assets/location_lg.png';
 import exp_date from '../../assets/exp_date_lg.png';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const FoodDetails = () => {
     const { _id, additionalNotes, foodStatus, donatorName, donatorImage, foodName, foodImage, foodQuantity, expireDate, created_at, pickupLocation, donatorNumber, donatorEmail } = useLoaderData();
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const foodModalRef = useRef(null);
+
+    const handleFoodRequestModalOpen = () => {
+        foodModalRef.current.showModal();
+    }
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors, isSubmitting },
+    } = useForm();
+
+    // Hnadle Request Food
+    const handleRequestFood = (requestData) => {
+        const newRequestData = { ...requestData, requesterEmail: user?.email, requesterName: user?.displayName, requesterImage: user?.photoURL, donatorEmail: donatorEmail, donatorName: donatorName, donatorImage: donatorImage, foodId: _id, foodStatus: 'Pending' };
+
+        axiosSecure.post('/food-request', newRequestData)
+            .then((data) => {
+                if (data.data.insertedId) {
+                    reset(); // reset form
+                    foodModalRef.current.close(); // Close Modal
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Food Request sent successfully!",
+                        showConfirmButton: false,
+                        timer: 2000
+                    })
+                }
+            })
+    }
 
     return (
         <section className='py-10 lg:py-20'>
@@ -86,10 +124,63 @@ const FoodDetails = () => {
                                 </div>
                             </div>
                         </div>
-                        <button className='button w-full'>Request Food</button>
+                        <button onClick={handleFoodRequestModalOpen} className='button w-full'>Request Food</button>
                     </div>
                 </div>
             </div>
+
+            {/* Request Food Modal */}
+            <dialog ref={foodModalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-semibold text-heading text-xl mb-4">Request Food</h3>
+                    <div>
+                        <form onSubmit={handleSubmit(handleRequestFood)}>
+                            {/* Location */}
+                            <div className='mb-4'>
+                                <label htmlFor="location" className='text-sm mb-2 inline-block'>Location</label>
+                                <input type="text"
+                                    {...register('location',
+                                        {
+                                            required: 'Location is required',
+                                        }
+                                    )}
+                                    className={`${errors.location ? 'border-red-500 focus:border-red-500 text-red-500' : 'border-dark-04 focus:border-ps-primary text-body'} w-full px-6 py-3.5 border  rounded-md focus:outline-0`} placeholder='123 Maple Street, Springfield, IL, 62701, USA' />
+                                <span className={`${errors.location ? 'block mt-1' : 'hidden'} text-[14px] text-red-500`}>{errors.location && errors.location.message}</span>
+                            </div>
+                            {/* Why Need Food  */}
+                            <div className='mb-2'>
+                                <label htmlFor="whyNeedFood" className='text-sm mb-2 inline-block'>Why Need Food</label>
+                                <textarea
+                                    {...register('whyNeedFood',
+                                        {
+                                            required: 'This field is required',
+                                        }
+                                    )}
+                                    cols="30" rows='4' className={`${errors.whyNeedFood ? 'border-red-500 focus:border-red-500 text-red-500' : 'border-dark-04 focus:border-ps-primary text-body'} w-full px-6 py-3.5 border  rounded-md focus:outline-0`} placeholder='Why Need Food'></textarea>
+                                <span className={`${errors.whyNeedFood ? 'block mt-1' : 'hidden'} text-[14px] text-red-500`}>{errors.whyNeedFood && errors.whyNeedFood.message}</span>
+                            </div>
+                            {/* Contact No */}
+                            <div className='mb-4'>
+                                <label htmlFor="location" className='text-sm mb-2 inline-block'>Contact No.</label>
+                                <input type="number"
+                                    {...register('contact',
+                                        {
+                                            required: 'Contact is required',
+                                        }
+                                    )}
+                                    className={`${errors.contact ? 'border-red-500 focus:border-red-500 text-red-500' : 'border-dark-04 focus:border-ps-primary text-body'} w-full px-6 py-3.5 border  rounded-md focus:outline-0`} placeholder='(555) 987-1234' />
+                                <span className={`${errors.contact ? 'block mt-1' : 'hidden'} text-[14px] text-red-500`}>{errors.contact && errors.contact.message}</span>
+                            </div>
+                            <button className='button mt-1' disabled={isSubmitting}>{isSubmitting ? <span className='loading loading-spinner loading-sm'></span> : 'Submit Request'}</button>
+                        </form>
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </section>
     );
 };
